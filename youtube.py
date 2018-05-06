7437c335444d1d0c4cd9555c1b4540d74b2e0e9c
Jordan Bottrell <bottrell@umich.edu>
2018-05-06 16:50:48 -0400

    big commit

--- youtube.py
+++ youtube.py
@@ -1,27 +1,12 @@
-'''
-#code to download youtube videos
-from pytube import YouTube
-yt = YouTube('https://www.youtube.com/watch?v=9bZkp7q19f0')
-stream = yt.streams.first()
-stream.download()
-'''
-
-
-'''
-#code to concatenate video clips
-from moviepy.editor import VideoFileClip, concatenate_videoclips
-clip1 = VideoFileClip("myvideo.mp4")
-clip2 = VideoFileClip("myvideo2.mp4").subclip(50,60)
-clip3 = VideoFileClip("myvideo3.mp4")
-final_clip = concatenate_videoclips([clip1,clip2,clip3])
-final_clip.write_videofile("my_concatenation.mp4")
-'''
-import requests
-import urllib
 import os
-from bs4 import BeautifulSoup
+import sys
 import json
+import glob
+import urllib
+import requests
 from pytube import YouTube
+from bs4 import BeautifulSoup
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 #client_id = '3w25cnut69wvv1za2ddw6bfe917150'
 #client_secret = 'vsdeigzx7pfynxat34gfi7942yrn6z'
 
@@ -42,9 +27,7 @@ def download_twitch_highlight(clip_origin):
 	url = obj[0]["source"]
 	filename = url.split("/")[-1]
 	THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-
 	my_file = os.path.join(THIS_FOLDER + "/videos", filename)
-
 
 	#url = """videos/https://clips-media-assets2.twitch.tv/AT-cm%7C28580236752-offset-17372-480.mp4"""
 	urllib.request.urlretrieve(url, my_file)
@@ -58,6 +41,28 @@ def download_youtube_clip(clip_origin):
 	stream.download('videos/')
 
 
+def make_vid():
+	#code to concatenate video clips
+	#go into videos folder, iterate through file names
+	print("made it")
+	filename_list = []
+	for filename in glob.iglob('videos/*.mp4'):
+		filename_list.append(filename)
 
-download_twitch_highlight("https://clips.twitch.tv/AbrasiveArtsyAnacondaDatBoi")
-download_youtube_clip('https://www.youtube.com/watch?v=beVsBZ7yYuA&feature=youtu.be')
+	video_clips = []
+	for x in filename_list:
+		video_clips.append(VideoFileClip(x))
+
+	final_clip = concatenate_videoclips(video_clips)
+	final_clip.write_videofile("exportedvideo.mp4")
+
+	'''clip1 = VideoFileClip("myvideo.mp4")
+	clip2 = VideoFileClip("myvideo2.mp4").subclip(50,60)
+	clip3 = VideoFileClip("myvideo3.mp4")
+	final_clip = concatenate_videoclips([clip1,clip2,clip3])
+	final_clip.write_videofile("my_concatenation.mp4")'''
+
+#download_twitch_highlight("https://clips.twitch.tv/AbrasiveArtsyAnacondaDatBoi")
+#download_youtube_clip('https://www.youtube.com/watch?v=beVsBZ7yYuA&feature=youtu.be')
+make_vid()
+
